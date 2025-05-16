93ee82de063c77c068fd9d18dab9c5a481029cc3
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-05-16 14:29:20 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
+# app/models.py
+from sqlalchemy import Column, Integer, String
+from app.database import Base
 
+class Warning(Base):
+    __tablename__ = "warnings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, nullable=False, index=True)
+    guild_id = Column(String, nullable=False, index=True)
+    reason = Column(String, nullable=True)
