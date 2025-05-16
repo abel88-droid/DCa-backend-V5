9b3e338b758a13f720104750edc94bc7fd311a60
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-05-16 14:21:09 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,24 @@
+# app/database.py
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # Neon DB URL from .env
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+Base = declarative_base()
+
+# Dependency for routes
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
